0c8a24a320b67d5845e144377c26167830af9638
Halit <halitguvenilir@gmail.com>
2021-02-21 01:30:04 +0300

    Product json file create and javascript map create list element

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import products from '../products.json'
+
 export default function Home() {
+  console.log('products', products);
   return (
     <div className={styles.container}>
       <Head>
@@ -15,39 +18,27 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className={styles.grid}>
+          {products.map(product => {
+            const { id, title, description, image, price } = product;
+            return (
+              <li key={id} className={styles.card}>
+                <a href="https://nextjs.org/docs">
+                  <img src={image} alt={title} />
+                  <h3>{title}</h3>
+                  <p>${ price }</p>
+                  <p>
+                    { description }
+                  </p>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
@@ -56,10 +47,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
